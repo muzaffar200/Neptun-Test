2a0e266d1d45031e215db334bed85345d769f115
muzaffar200 <mmustafayev004@gmail.com>
2024-11-02 13:59:01 +0400

    son

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Endirimler from './Endirimler'
-import YalnizNeptunda from './yalnizNeptunda'
 
 function Main() {
     return (
         <>
             <Endirimler />
-            <YalnizNeptunda />
         </>
     )
 }
